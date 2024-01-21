456b582b45b9be93f93a7e6c592a82fdc952dd5e
naki <naki@student.42seoul.kr>
2024-01-21 14:46:30 +0900

    fin

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,13 @@
 function goodWork(position) {
 	const lat = position.coords.latitude;
-	const lng = position.coords.longitude;
-	console.log("You live in ", lat, lng);
+	const lon = position.coords.longitude;
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+	fetch(url).then(response => response.json()).then(data => {
+		const city = document.querySelector("#weather span:first-child");
+		const weather = document.querySelector("#weather span:last-child");
+		city.innerText = data.name;
+		weather.innerText = `\n${data.weather[0].main} / ${data.main.temp}℃`;
+	});
 }
 
 function errorWork() {
